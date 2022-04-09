c27f9e0f1cd16ad8011692cde1b1ad90ae328ccf
Lenovo <Lenovo@DESKTOP-4MMCC21>
2022-04-09 18:01:35 -0300

    ok

---
+++ SIG1/src/main/java/com/fatec/sig1/ports/ClienteRepository.java
@@ -0,0 +1,16 @@
+package com.fatec.sig1.ports;
+
+import java.util.List;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import com.fatec.sig1.model.Cliente;
+
+@SuppressWarnings("unused")
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+	Optional<Cliente> findByCpf(String cpf);
+
+	List<Cliente> findAllByNomeIgnoreCaseContaining(String nome);
+}
